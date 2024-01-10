a1ce6cf46d51d89528e517796a980e613cede57d
Homayoun <blodbyte.c@gmail.com>
2024-01-11 02:47:59 +0330

    Update the slider and use the swiper instead of the pure

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,12 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, EffectFade , Autoplay} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import "swiper/css/autoplay";
+import { useEffect, useState } from "react";
 
 const data = [
   {
@@ -21,35 +27,56 @@ const data = [
 ];
 
 const Slider = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
+  // const [currentSlide, setCurrentSlide] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(
-      () =>
-        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
-      4000
-    );
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(
+  //     () =>
+  //       setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+  //     4000
+  //   );
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
-      {/* TEXT CONTAINER */}
-      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
-          {data[currentSlide].title}
-        </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
-      </div>
-      {/* IMAGE CONTAINER */}
-      <div className="w-full flex-1 relative">
-        <Image
-          src={data[currentSlide].image}
-          alt=""
-          fill
-          className="object-cover"
-        />
-      </div>
+    <div>
+      <Swiper
+        modules={[Navigation, EffectFade , Autoplay]}
+        effect="fade"
+        spaceBetween={5}
+        slidesPerView={1}
+        navigation
+        loop={true}
+        autoplay={{ delay: 3000 }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
+              <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+                <h1
+                  className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl"
+                  title={item.title}
+                >
+                  {item.title}
+                </h1>
+                <button className="bg-red-500 text-white py-4 px-8">
+                  Order Now
+                </button>
+              </div>
+              <div className="w-full flex-1 relative">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
